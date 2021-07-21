98a1797fc121228d4f73a8a3fd22eaec093581ae
David-Lin665 <83898249+David-Lin665@users.noreply.github.com>
2021-07-21 10:39:41 +0800

    succeed to let the tile run delay
    coroutine added

--- Alchemist Myths/Assets/Scripts/DestructSpawn.cs
+++ Alchemist Myths/Assets/Scripts/DestructSpawn.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,37 +8,49 @@ public class DestructSpawn : MonoBehaviour
 {
     public Tilemap destructibleTilemap;
     public LayerMask targetLayerMask;
-    private Vector3 mousePosition;
     public float health = 10;
     public float damage = 2;
     private RaycastHit2D hit;
+    private Vector3 mouseWorldPosition;
+    private Color tileColor;
     private void Awake() 
     {
         destructibleTilemap = GetComponent<Tilemap>();
     }
     void Start()
     {
-        StartCoroutine(BreaktheBlock());
+        StartCoroutine(BreakBlocks());
     }
     void Update()
     {
-        mousePosition = Input.mousePosition;
+        Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = Camera.main.nearClipPlane;
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         hit = Physics2D.Raycast(new Vector2(mouseWorldPosition.x,mouseWorldPosition.y),Vector2.zero,0,targetLayerMask);
+        if(health <= 0)
+            destructibleTilemap.SetTile(destructibleTilemap.WorldToCell(mouseWorldPosition),null);
     }
-    IEnumerator BreaktheBlock()
+    
+    IEnumerator BreakBlocks()
     {
-        Debug.Log("coroutine starts");
+        tileColor = destructibleTilemap.GetColor(destructibleTilemap.WorldToCell(mouseWorldPosition));
         while (true)
         {
-            Debug.Log("check");
-            if(hit&&Input.GetMouseButtonDown(0))
-            {
-                health -= damage;
-                Debug.Log(health);
-            }
-            yield return new WaitForSeconds(10);
+            yield return new WaitUntil(()=>hit&&Input.GetMouseButton(0));
+            health--;
+            Debug.Log(health);
+            yield return StartCoroutine(Fade());
+            yield return new WaitForSeconds(1);
         }
     }
+
+    IEnumerator Fade()
+    {
+        tileColor.a -= 0.1f;
+        Debug.Log(tileColor);
+        destructibleTilemap.SetColor(destructibleTilemap.WorldToCell(mouseWorldPosition),tileColor);
+        yield return null;
+    }
+        
+    
 }
