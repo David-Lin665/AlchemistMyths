e79cf34090b19e9cb2755380bf7a169bc071c097
David-Lin665 <83898249+David-Lin665@users.noreply.github.com>
2021-07-27 09:58:26 +0800

    fixed done

---
+++ Alchemist Myths/Assets/scripts/InventoryScripts/MoveBag.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MoveBag : MonoBehaviour, IDragHandler
+{
+    public RectTransform curruntRect;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        curruntRect.anchoredPosition += eventData.delta;
+    }
+
+    void Awake()
+    {
+        curruntRect = GetComponent<RectTransform>();
+    }
+}
---
+++ Alchemist Myths/Assets/scripts/InventoryScripts/Slot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public int slotID;
+    public Item slotItem;
+    public Image slotImage;
+    public Text slotnum;
+    public GameObject itemInSlot;
+    public string slotInfo;
+
+    public void SlotOnclick()
+    {
+        InventoryManager.UpdateItemInfo(slotInfo);
+    }
+
+    public void SetSlot(Item item)
+    {
+        if(item == null)
+        {
+            itemInSlot.SetActive(false);
+            return;
+        }
+        slotItem = item;
+        slotImage.sprite = item.icon;
+        slotnum.text = item.itemheld.ToString();
+        slotInfo = item.description;
+    }
+}
